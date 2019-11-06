8e8638b09d96179cbcb07165c685eba7ce1306ae
Thijs <thijss@pm.me>
2019-11-06 14:37:57 +0100

    pylint update

--- cmd/pylint.sh
+++ cmd/pylint.sh
@@ -2,5 +2,7 @@
 clear
 echo "PYLINT"
 touch apps/__init__.py
-python $(which pylint) --rcfile=.pylintrc apps/ 
+python $(which pylint) --rcfile=.pylintrc apps/
+exit_code=$? 
 rm apps/__init__.py
+exit $exit_code
