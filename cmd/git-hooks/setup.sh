805c106b0345339f5cd88bb2aca9e95886527b8f
Thijs <thijss@pm.me>
2019-11-06 15:21:49 +0100

    update pre-commit setup file

--- cmd/git-hooks/setup.sh
+++ cmd/git-hooks/setup.sh
@@ -1,6 +1,7 @@
 # #!/bin/bash
-# setup for githooks
+# setup for githooks. 
+# IMPORTANT: Make sure to run this command from the root folder of the repository
 
 # pre-commit
 rm .git/hooks/pre-commit
-ln -s cmd/git-hooks/pre-commit .git/hooks/pre-commit
+ln -s ../../cmd/git-hooks/pre-commit .git/hooks/pre-commit
