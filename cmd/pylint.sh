d3a2c8329ca752b8e299dad43588dfb7dba056bf
Thijs <thijss@pm.me>
2019-11-06 15:08:09 +0100

    update pylint

--- cmd/pylint.sh
+++ cmd/pylint.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NOCOLOR='\033[0m'
+
 clear
-echo "PYLINT"
 touch apps/__init__.py
 python $(which pylint) --rcfile=.pylintrc apps/
 exit_code=$? 
 rm apps/__init__.py
+
+if [[ exit_code -ne 0 ]] ; then
+    echo -e "${RED}Pylint: errors${NOCOLOR}";
+    exit $exit_code;
+fi
+
+echo -e "${GREEN}Pylint: passed${NOCOLOR}"
+
 exit $exit_code
