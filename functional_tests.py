46a91db5f1b6e9ec9989e3e10dae9367467e7c31
Thijs <thijss@pm.me>
2019-11-26 15:30:54 +0100

    add new function tests

--- functional_tests.py
+++ functional_tests.py
@@ -9,15 +9,17 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):  
         self.browser.quit()
 
-    def test_can_start_a_list_and_retrieve_it_later(self):  
+    def test_can_create_an_account(self):  
         # Michiel has heard about a cool new website to check out match statistics for his football team.
         # He goes to checkout the homepage.
         self.browser.get('http://localhost:8000')
 
-        # He notices the page title and header mention 'Football'
-        self.assertIn('Football', self.browser.title)
-        # He realizes he needs to create an account first, for he is immediately taken to a login page.
-        self.assertIn('login', self.browser.url)
+        # He realizes he needs to create an account first, for he is immediately redirected to a login page.
+        # self.assertIn('Login', self.browser.title)
+        # self.assertIn('login', self.browser.current_url)
+
+        # He goes to the registration page.
+        # self.assertIn('Registration', self.browser.title)
 
         self.fail('Finish the test!')
 
