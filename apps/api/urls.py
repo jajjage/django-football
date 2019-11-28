9057fac3550cfd61923de1e592fa94722d646f1f
Thijs Baaijen <thijss@pm.me>
2019-11-28 12:23:37 +0100

    delete schemaview

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -5,14 +5,11 @@ from rest_framework.routers import DefaultRouter
 
 from .matches.viewsets import MatchViewSet
 from .players.viewsets import PlayerViewSet
-from .views import SchemaView
 
 router = DefaultRouter()
 router.register(r'players', PlayerViewSet)
 router.register(r'matches', MatchViewSet)
 
 urlpatterns = [
-    path('', SchemaView.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('', include(router.urls)),
-    path('redoc', SchemaView.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
